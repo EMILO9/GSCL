01c1f260d205525eb5e4ea3fbc0f62c8d873f7a6
EMILO9 <emilberolsen@gmail.com>
2023-05-15 12:30:33 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/repo/",
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
